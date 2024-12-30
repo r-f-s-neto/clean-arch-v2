598709169915a2da7ebc28ff21f02c0a77f53ac4
Raimundo Neto <freeshopeletronicos@gmail.com>
2024-12-30 14:18:28 -0300

    feat: ensure RemoteAuthentication calls HttpPostClient with correct url

---
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -0,0 +1,13 @@
+import { IHttpPostClient } from "@/data/protocols/http/http-post-client";
+
+export class RemoteAuthentication {
+  constructor(
+    private readonly url: string,
+    private readonly httpPostClient: IHttpPostClient
+  ) {}
+
+  async auth(): Promise<void> {
+    this.httpPostClient.post(this.url);
+    return Promise.resolve();
+  }
+}
