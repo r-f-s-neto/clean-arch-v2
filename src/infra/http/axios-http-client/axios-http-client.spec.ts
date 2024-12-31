9516d53e8f201dff1bbf483ac57101a3bdad520a
Raimundo Neto <freeshopeletronicos@gmail.com>
2024-12-31 15:29:55 -0300

    feat: ensure axios have been called with correct url

---
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { AxiosHttpClient } from "./axios-http-client";
+import faker from "faker";
+
+const makeSut = () => {
+  const sut = new AxiosHttpClient();
+
+  return { sut };
+};
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe("AxiosHttpClient", () => {
+  it("Should call axios with correct url", async () => {
+    const { sut } = makeSut();
+    const url = faker.internet.url();
+    await sut.post({ url });
+    expect(mockedAxios).toHaveBeenCalledWith(url);
+  });
+});
