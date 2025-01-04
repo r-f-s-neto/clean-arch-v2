7f684d257057d6b012221bb6d71843cb6ee3e2b0
Raimundo Neto <freeshopeletronicos@gmail.com>
2025-01-04 12:13:00 -0300

    feat: create of router

---
+++ src/presentation/components/router/router.tsx
@@ -0,0 +1,19 @@
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "@/presentation/styles/global.scss";
+
+const Login = lazy(() => import("@/presentation/pages/login"));
+
+const Router: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<div>carregando...</div>}>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
